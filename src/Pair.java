05cd8c7657b166181089b5607ef910faa1e6c0d0
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2015-02-20 02:26:58 +0200

    finish playfair encryption algorithm
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

---
+++ src/Pair.java
@@ -0,0 +1,29 @@
+/**
+ * Created by AhmedA on 2/20/2015.
+ */
+public class Pair {
+    private int x;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    private int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
