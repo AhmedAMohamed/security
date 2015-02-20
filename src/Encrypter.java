17062278515eb493a7c7576b8bd232426082c263
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2015-02-20 02:26:20 +0200

    finish playfair encryption algorithm
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
  * Created by AhmedA on 2/19/2015.
  */
 public class Encrypter {
-    public static char[] alpha;
+    private static char[] alpha;
 
     public static char[] EncryptByCaesar(int key, char[] plainText){
         char[] cipher = new char[plainText.length];
@@ -24,17 +24,17 @@ public class Encrypter {
     }
     public static char[] EncryptByPlayFair(char[] key, char[] plainText){
         char[][] keyMatrix = new char[5][5];
-        char[] cipher = new char[plainText.length];
+        char[] cipher;
         fillFullMatrix();
         int startX = 0 , startY = 0;
         boolean first = true;
         char[] currentKey = generateValidKey(key);
 
-        System.out.println(currentKey);
+        //System.out.println(currentKey);
         for (int i = 0; i < currentKey.length ; i++) {
             markAlpha(currentKey[i]);
         }
-        System.out.println(alpha);
+        //System.out.println(alpha);
         for (int i = 0, k = 0 ; i < 5 ; i++) {
             for (int j = 0; j < 5 ; j++) {
                 if(k >= currentKey.length){
@@ -49,7 +49,6 @@ public class Encrypter {
                     k++;
                 }
             }
-
         }
         ArrayList<Character> currentAlpha = new ArrayList<Character>();
         for (int i = 0; i < alpha.length ; i++) {
@@ -65,10 +64,60 @@ public class Encrypter {
                 keyMatrix[i][j] += currentAlpha.get(k);
             }
         }
-        for (int i = 0; i < plainText.length ; i++) {
-        //    findLOcation();
+        plainText = createValidPlainText(plainText);
+        cipher = new char[plainText.length];
+        for (int i = 0; i < plainText.length ; i+=2) {
+            Pair charLocationFirst = findLocation(keyMatrix, plainText[i]);
+            Pair charLocationSecond = findLocation(keyMatrix,plainText[i+1]);
+            if(charLocationFirst.getX() == charLocationSecond.getX()){
+                cipher[i] = keyMatrix[charLocationFirst.getX()][(charLocationFirst.getY()+1)%5];
+                cipher[i+1] = keyMatrix[charLocationFirst.getX()][(charLocationSecond.getY()+1)%5];
+            }
+            else if(charLocationFirst.getY() == charLocationSecond.getY()){
+                cipher[i] = keyMatrix[(charLocationFirst.getX()+1)%5][charLocationSecond.getY()];
+                cipher[i+1] = keyMatrix[(charLocationSecond.getX()+1)%5][charLocationSecond.getY()];
+            }
+            else{
+                cipher[i] = keyMatrix[charLocationFirst.getX()][charLocationSecond.getY()];
+                cipher[i+1] = keyMatrix[charLocationSecond.getX()][charLocationFirst.getY()];
+            }
         }
-        return key;
+        return cipher;
+    }
+
+    private static char[] createValidPlainText(char[] plainText) {
+        ArrayList<Character> plain = new ArrayList<Character>();
+        char[] validPlainText;
+        for (int i = 0; i < plainText.length ; i++) {
+            plain.add(plainText[i]);
+        }
+        for (int i = 0; i < plainText.length ; i+=2) {
+            if(plain.get(i) == plain.get(i+1)){
+               plain.add(i+1,'x');
+            }
+        }
+        if(plain.size() % 2 != 0){
+            plain.add('x');
+        }
+        validPlainText = new char[plain.size()];
+        for (int i = 0; i <plain.size() ; i++) {
+            validPlainText[i] = plain.get(i);
+        }
+        return validPlainText;
+    }
+
+    private static Pair findLocation(char[][] keyMatrix, char c) {
+        Pair current = new Pair(0,0);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 ; j++) {
+                if(c == keyMatrix[i][j]){
+                    current.setX(i);
+                    current.setY(j);
+                    break;
+                }
+            }
+        }
+        return current;
     }
 
     private static void fillFullMatrix() {
@@ -119,6 +168,6 @@ public class Encrypter {
     public static void main(String[] args){
         String x = "playfairexample";
         char[] y = x.toCharArray();
-        EncryptByPlayFair(y,y);
+        System.out.println();
     }
 }
